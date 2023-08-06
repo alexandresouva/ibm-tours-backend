0626b2d7b4d6eea8dfc2243a845c561fdeb1bee2
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-08-06 16:48:09 -0300

    initial commit

---
+++ src/main/java/com/example/challengeibm/ChallengeIbmApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengeibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeIbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeIbmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/challengeibm/ChallengeIbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengeibm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeIbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
